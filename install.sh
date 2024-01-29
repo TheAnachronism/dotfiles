73c6e87196aa256d1aaf22d9111c311ea10c8a38
TheAnachronism <TheAnachronism@outlook.com>
2024-01-29 14:39:37 +0100

    Added the copy of the custom .zshrc file and moved the oh-my-zsh install into its own function.

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+DOTFILES_DIR="$HOME/.dotfiles"
+
+install_oh_my_zsh() {
+  # Install Oh-My-Zsh
+  if [ ! -d "$HOME/.oh-my-zsh/" ] 
+  then
+    echo "Installing Oh-My-Zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  else
+    echo "Oh-My-Zsh is already installed."
+  fi
+}
+
 echo "Updating and upgrading your system..."
 sudo apt update && sudo apt upgrade -y
 
@@ -11,16 +24,12 @@ else
   echo "ZSH is already installed."
 fi
 
-# Install Oh-My-Zsh
-if [ ! -d "$HOME/.oh-my-zsh/" ] 
-then
-  echo "Installing Oh-My-Zsh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-else
-  echo "Oh-My-Zsh is already installed."
-fi
-
 # Set ZSH as default shell
 chsh -s $(which zsh)
 
+install_oh_my_zsh
 
+# Setting up the custom .zshrc file
+cp $DOTFILES_DIR/zshrc $HOME/.zshrc
+
+# Dotfiles are complete.

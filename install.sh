bb52486e957efbe19c6a10e2c36b0884d9bf1c1b
TheAnachronism <TheAnachronism@outlook.com>
2024-01-29 14:48:55 +0100

    Added a testing docker image and switched to unattended mode for the oh-my-zsh install.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ install_oh_my_zsh() {
   if [ ! -d "$HOME/.oh-my-zsh/" ] 
   then
     echo "Installing Oh-My-Zsh"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
   else
     echo "Oh-My-Zsh is already installed."
   fi
